0e9d1699bcb70ace3aec9aef1f62000a09e3284b
Eliott <eliott.dagostinoz@universite-paris-saclay.fr>
2026-04-15 16:41:52 +0200

    fix: close connection on failed handshake

--- scripts/server/GameClient.cs
+++ scripts/server/GameClient.cs
@@ -8,71 +8,71 @@ using System.Threading.Channels;
 using System.Threading.Tasks;
 
 public partial class GameClient : IDisposable {
-    private readonly TcpClient tcpClient;
-    private readonly int id;
-    private readonly TcpMessageFramer tcpMessageFramer;
-    private bool _disposed; // Pour le Garbage Controller
+	private readonly TcpClient tcpClient;
+	private readonly int id;
+	private readonly TcpMessageFramer tcpMessageFramer;
+	private bool _disposed; // Pour le Garbage Controller
 
-    public GameClient(TcpClient tcpClient, int id) {
-        this.tcpClient = tcpClient;
-        // On délègue la lecture des messages à une classe utilitaire pour s'assurer qu'on réspècte le protocole !
-        this.tcpMessageFramer = new(tcpClient.GetStream()); 
-        this.id = id;
-    }
+	public GameClient(TcpClient tcpClient, int id) {
+		this.tcpClient = tcpClient;
+		// On délègue la lecture des messages à une classe utilitaire pour s'assurer qu'on réspècte le protocole !
+		this.tcpMessageFramer = new(tcpClient.GetStream()); 
+		this.id = id;
+	}
 
-    public int GetId() { return this.id; }
+	public int GetId() { return this.id; }
 
-    public async Task<bool> PerformHandshake() {
-        // Première étape: Le serveur dit "PIXELWAR 1.0" (il s'annonce et sa version de protocole)
-        /*
-            Exemple de Handshake:
-            server: PIXELWAR 1.0
-            client: REMOTE 1.0
-            server: WELCOME {id}
-        */
-        // On envoie PIXELWAR 1.0
-        await tcpMessageFramer.SendAsync("PIXELWAR 1.0");
+	public async Task<bool> PerformHandshake() {
+		// Première étape: Le serveur dit "PIXELWAR 1.0" (il s'annonce et sa version de protocole)
+		/*
+			Exemple de Handshake:
+			server: PIXELWAR 1.0
+			client: REMOTE 1.0
+			server: WELCOME {id}
+		*/
+		// On envoie PIXELWAR 1.0
+		await tcpMessageFramer.SendAsync("PIXELWAR 1.0");
 
-        string? clientVersion = await tcpMessageFramer.ReceiveAsync();
-        if (clientVersion == null) return false;
-        if (clientVersion != "REMOTE 1.0") return false;
+		string? clientVersion = await tcpMessageFramer.ReceiveAsync();
+		if (clientVersion == null) return false;
+		if (clientVersion != "REMOTE 1.0") return false;
 
-        await tcpMessageFramer.SendAsync($"WELCOME {id}");
+		await tcpMessageFramer.SendAsync($"WELCOME {id}");
 
-        // On a réussi notre handshake à partir d'ici !!
-        return true;
-    }
+		// On a réussi notre handshake à partir d'ici !!
+		return true;
+	}
 
-    public bool IsConnected => tcpClient.Connected && !_disposed;
+	public bool IsConnected => tcpClient.Connected && !_disposed;
 
-    /// <summary>
-    /// Graceful shutdown
-    /// </summary>
-    /// <exception cref="NotImplementedException"></exception>
-    public void Dispose() {
-        if (_disposed) return;
-        tcpClient.Close();
-        _disposed = true;
-    }
+	/// <summary>
+	/// Graceful shutdown
+	/// </summary>
+	/// <exception cref="NotImplementedException"></exception>
+	public void Dispose() {
+		if (_disposed) return;
+		tcpClient.Close();
+		_disposed = true;
+	}
 
-    /// <summary>
-    /// Envoie un paquet au client
-    /// </summary>
-    public async Task SendAsync(String message) {
-        Packet packet = new Packet(PacketType.Message, message);
-        byte[] serialized = packet.Serialize();
-        await tcpMessageFramer.SendAsync(serialized.Stringify());
-    }
+	/// <summary>
+	/// Envoie un paquet au client
+	/// </summary>
+	public async Task SendAsync(String message) {
+		Packet packet = new Packet(PacketType.Message, message);
+		byte[] serialized = packet.Serialize();
+		await tcpMessageFramer.SendAsync(serialized.Stringify());
+	}
 
-    /// <summary>
-    /// Reçoit un paquet du client
-    /// </summary>
-    public async Task<Packet?> ReceiveAsync() {
-        String message = await tcpMessageFramer.ReceiveAsync();
-        if (message == null) return null;
-        Packet p = Packet.Deserialize(Encoding.UTF8.GetBytes(message));
-        return p;
-    }
+	/// <summary>
+	/// Reçoit un paquet du client
+	/// </summary>
+	public async Task<Packet?> ReceiveAsync() {
+		String message = await tcpMessageFramer.ReceiveAsync();
+		if (message == null) return null;
+		Packet p = Packet.Deserialize(Encoding.UTF8.GetBytes(message));
+		return p;
+	}
 
 
 }
